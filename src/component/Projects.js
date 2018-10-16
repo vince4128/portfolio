948f6537b79c2d3be2ba1afa9c9503d5c6cd6d1c
vince4128 <vrlt.vincent@gmail.com>
2018-10-16 17:51:30 +0200

    content added

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -9,10 +9,53 @@ class Projects extends Component{
     render(){
         return(
             <section id="projects" className="section--projects o-projects">
-                <article className="o-projects__webapp"></article>
-                <article className="o-projects__sitemu"></article>
-                <article className="o-projects__sitelars"></article>
-                <article className="o-projects__newproject"></article>
+                <article className="o-projects__webapp m-project m-project--wide">
+                    <h2>Blabla</h2>
+                    <div className="m-project__info">
+                        <p className="m-project__info__legend">blablbalbalbabla balbalbalba</p>
+                        <ul className="m-project__info__tools">
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                        </ul>
+                    </div>
+                </article>
+                <article className="o-projects__sitemu m-project">
+                    <h2>Blabla</h2>
+                    <div className="m-project__info">
+                        <p className="m-project__info__legend">blablbalbalbabla balbalbalba</p>
+                        <ul className="m-project__info__tools">
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                        </ul>
+                    </div>
+                </article>
+                <article className="o-projects__sitelars m-project">
+                    <h2>Blabla</h2>
+                    <div className="m-project__info">
+                        <p className="m-project__info__legend">blablbalbalbabla balbalbalba</p>
+                        <ul className="m-project__info__tools">
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                        </ul>
+                    </div>
+                </article>
+                <article className="o-projects__newproject m-project">
+                    <h2>Blabla</h2>
+                    <div className="m-project__info">
+                        <p className="m-project__info__legend">blablbalbalbabla balbalbalba</p>
+                        <ul className="m-project__info__tools">
+                            <li>tool</li>
+                            <li>tool</li>
+                            <li>tool</li>
+                        </ul>
+                    </div>
+                </article>
             </section>
         )
     }
--- src/component/Tools.js
+++ src/component/Tools.js
@@ -2,8 +2,22 @@ import React, { component } from 'react';
 
 const Tools = (props) => {
     return(
-        <section id="tools" className="section--tools">
-            <h1>Tools</h1>
+        <section id="tools" className="section--tools o-tools">
+            <article className="o-tools__js">
+                <h1>JS</h1>
+            </article>
+            <article className="o-tools__css">
+                <h1>CSS</h1>
+            </article>
+            <article className="o-tools__html5">
+                <h1>HTML5</h1>
+            </article>
+            <article className="o-tools__toolsdetail">
+                <h1>toolsdetail</h1>
+            </article>
+            <article className="o-tools__othertool">
+                <h1>othertool</h1>
+            </article>
         </section>
     )
 }
