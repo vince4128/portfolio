5dc16c1cee085422af91b2d2528f24dd15b7e1db
vince4128 <vrlt.vincent@gmail.com>
2018-11-12 17:21:38 +0100

    contact from field added

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Contact = (props) => {
     return(
-        <section id="contact" className="section--contact">
+        <section id="contact" className="section--contact o-contact">            
+            <form className="o-contact__form">
             <h1>Contact</h1>
+                <fieldset>                    
+                    <label for="nom">Nom</label>
+                    <input type="text"></input>
+                    <label>Prénom</label>
+                    <input type="text"></input>
+                    <label>Mail de contact</label>
+                    <input type="mail"></input>
+                </fieldset>
+                <fieldset>
+                    <label>Message</label>
+                    <textarea></textarea>
+                </fieldset>
+                <button type="submit">envoyer</button>
+            </form>
             <div className="polygon">
                 <svg preserveAspectRatio="none" viewBox="0 0 100 100">
                     <polygon points="0,100 100,0 0,0" opacity="1"></polygon>
